bb488af878f1004a2a59c3d373444368e831a334
Ricardo Ramirez <ricardo.ramirezlara@gmail.com>
2018-06-24 21:18:19 -0600

    Set fianl hero content

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import meetup from '../img/meetup.svg';
 
 const IndexPage = () => (
-  <section class="section">
-    <h1 className="title is-1 has-text-black has-text-weight-bold">
-      Comunidad de JavaScript <br /> en El Salvador 🇸🇻
-    </h1>
-    <a class="button is-primary is-outlined is-large">
-      <span>Registrarme</span>
-    </a>
-  </section>
+  <div>
+    <section class="section has-text-centered">
+      <h1 className="title is-1 has-text-black has-text-weight-bold">
+        Comunidad de JavaScript <br /> en El Salvador
+      </h1>
+      <a
+        href="https://www.meetup.com/es-ES/horchatajs/"
+        class="button is-primary is-large"
+      >
+        <span>Registrate</span>
+      </a>
+    </section>
+  </div>
 );
 
 export default IndexPage;
