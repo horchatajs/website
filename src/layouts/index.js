e6e379299acb1e79b7f4605987d8cef7f5f71f9c
Ricardo Ramirez <ricardo.ramirezlara@gmail.com>
2018-07-29 23:53:45 -0600

    Add global meta tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import { withPrefix } from 'gatsby-link';
 
 import Navbar from '../components/Navbar';
 import './all.scss';
@@ -9,8 +10,29 @@ const TemplateWrapper = ({ children }) => (
   <div className="container">
     <Helmet>
       <meta charSet="utf-8" />
-      <title>HorchataJS – Comunidad de JavaScript en El Salvador</title>
-      <link rel="canonical" href="https://www.horchatajs.com" />
+      {/* Facebook Open Graph */}
+      <meta property="og:url" content="https://www.horchatajs.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="HorchataJS" />
+      <meta property="og:image" content={withPrefix('/img/share-logo.png')} />
+      <meta property="og:image:width" content="200" />
+      <meta property="og:image:height" content="200" />
+      <meta
+        property="og:description"
+        content="Comunidad de JavaScript en El Salvador"
+      />
+      <meta property="og:site_name" content="HorchataJS" />
+
+      {/* Twitter Card */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@horchatajs" />
+      <meta name="twitter:url" content="https://www.horchatajs.com" />
+      <meta name="twitter:title" content="HorchataJS" />
+      <meta
+        name="twitter:description"
+        content="Comunidad de JavaScript en El Salvador"
+      />
+      <meta name="twitter:image" content={withPrefix('/img/share-logo.png')} />
     </Helmet>
     <Navbar />
     {children()}
