51d46d89eb8250da661fbec69b881c595e383e64
Ricardo Ramirez <ricardo.ramirezlara@gmail.com>
2018-08-06 13:18:40 -0600

    Use global graphql metadata

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,43 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { withPrefix } from 'gatsby-link';
 
 import Navbar from '../components/Navbar';
 import './all.scss';
 
-const TemplateWrapper = ({ children }) => (
+const TemplateWrapper = ({
+  children,
+  data: {
+    site: { siteMetadata: meta },
+  },
+}) => (
   <div className="container">
     <Helmet>
       <meta charSet="utf-8" />
       {/* Facebook Open Graph */}
-      <meta property="og:url" content="https://www.horchatajs.com" />
+      <meta property="og:url" content={meta.siteUrl} />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="HorchataJS" />
+      <meta property="og:title" content={meta.title} />
       <meta
         property="og:image"
-        content="https://www.horchatajs.com/img/share-logo.png"
+        content={`${meta.siteUrl}/img/share-logo.png"`}
       />
       <meta property="og:image:width" content="200" />
       <meta property="og:image:height" content="200" />
-      <meta
-        property="og:description"
-        content="Comunidad de JavaScript en El Salvador"
-      />
-      <meta property="og:site_name" content="HorchataJS" />
+      <meta property="og:description" content={meta.description} />
+      <meta property="og:site_name" content={meta.title} />
 
       {/* Twitter Card */}
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@horchatajs" />
-      <meta name="twitter:url" content="https://www.horchatajs.com" />
-      <meta name="twitter:title" content="HorchataJS" />
-      <meta
-        name="twitter:description"
-        content="Comunidad de JavaScript en El Salvador"
-      />
+      <meta name="twitter:site" content={meta.siteUrl} />
+      <meta name="twitter:url" content={meta.siteUrl} />
+      <meta name="twitter:title" content={meta.title} />
+      <meta name="twitter:description" content={meta.description} />
       <meta
         name="twitter:image"
-        content="https://www.horchatajs.com/img/share-logo.png"
+        content={`${meta.siteUrl}/img/share-logo.png"`}
       />
     </Helmet>
     <Navbar />
@@ -50,3 +48,17 @@ TemplateWrapper.propTypes = {
 };
 
 export default TemplateWrapper;
+
+export const query = graphql`
+  query LayoutQuery {
+    site {
+      siteMetadata {
+        title
+        description
+        siteUrl
+        twitterHandler
+        facebookHandler
+      }
+    }
+  }
+`;
