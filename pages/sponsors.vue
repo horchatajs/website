f6212a2176875b1841a45bab02fd1e0d87394630
Diego Melgar <dlopez.project.sv@gmail.com>
2019-07-28 21:43:44 -0600

    :art: Sponsors page

--- pages/sponsors.vue
+++ pages/sponsors.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>
-    Patrocinios - HorchataJS
+  <div class="flex flex-col items-center w-full justify-between xl:flex-row">
+    <img
+      class="-mt-32 sm:mt-0 xl:ml-32"
+      src="~/assets/images/sponsor.svg"
+      alt="Join"
+      width="700">
+    <div class="flex flex-col text-center items-center -mt-40 sm:mt-4 xl:items-end xl:text-right xl:mr-32">
+      <div class="font-bold text-2xl md:text-5xl lg:text-6xl xl:leading-tight">
+        Patrocinio
+      </div>
+
+      <div class="tracking-wide text-sm sm:text-lg font-light mt-8 xl:mt-12">
+        ¿Te interesa patrocinar un evento de la <br>comunidad? ¡Escribínos!
+      </div>
+
+      <app-button link="http://slack.horchatajs.com" class="mt-8">
+        <template v-slot:button-text>
+          <span class="normal-case">
+            Contactanos
+          </span>
+        </template>
+        <template v-slot:icon>
+          <span>
+            <i class="ml-3 ic-arrow" />
+          </span>
+        </template>
+      </app-button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
-</script>
+import AppButton from '~/components/AppButton'
 
-<style>
-</style>
+export default {
+  components: {
+    AppButton
+  }
+}
+</script>
