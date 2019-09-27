68d8c4ee636774d4c709892233c4c60a73172274
Diego Melgar <diego.melgar.sv@gmail.com>
2019-09-27 16:54:45 -0600

    :art: Nuxt link on toolbar logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,13 @@
   <div class="p-8 h-screen bg-grey-lightest max-w-7xl mx-auto">
     <!-- Toolbar -->
     <div class="flex justify-between items-center mb-6">
-      <img
-        loading="lazy"
-        class="h-10"
-        src="~/assets/images/full-logo.svg"
-        alt="full-logo">
+      <nuxt-link to="/">
+        <img
+          loading="lazy"
+          class="h-10"
+          src="~/assets/images/full-logo.svg"
+          alt="full-logo">
+      </nuxt-link>
       <responsive-menu class="inline lg:hidden" />
     </div>
     <!-- Content -->
